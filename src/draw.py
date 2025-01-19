087152154b1c2d9cfd2f2cc8a1c2577f537d9892
sultanothman <soltanotman@gmail.com>
2025-01-19 19:10:46 +0200

    create cell class, using this class we are going to draw the maze

--- src/draw.py
+++ src/draw.py
@@ -13,4 +13,39 @@ class Line:
 
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point_a.x, self.point_a.y, self.point_b.x, self.point_b.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, left_wall=True, right_wall=True, top_wall=True, bottom_wall=True, x1=0, y1=0, x2=0, y2=0, window=None):
+        self.has_left_wall = left_wall
+        self.has_right_wall =right_wall
+        self.has_top_wall = top_wall
+        self.has_bottom_wall = bottom_wall
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 =y2
+        self._win = window
+
+    def draw(self):
+        top_left_corner = Point(self._x1, self._y1)
+        top_right_corner = Point(self._x2, self._y1)
+        bottom_left_corner = Point(self._x1, self._y2)
+        bottom_right_corner = Point(self._x2, self._y2)
+
+        if self.has_top_wall:
+            line = Line(top_left_corner, top_right_corner)
+            self._win.draw_line(line, "black")
+
+        if self.has_left_wall:
+            line = Line(top_left_corner, bottom_left_corner)
+            self._win.draw_line(line, "black")
+
+        if self.has_right_wall:
+            line = Line(top_right_corner, bottom_right_corner)
+            self._win.draw_line(line, "black")
+
+        if self.has_bottom_wall:
+            line = Line(bottom_left_corner, bottom_right_corner)
+            self._win.draw_line(line, "black")
+
         
--- src/main.py
+++ src/main.py
@@ -1,19 +1,51 @@
 from graphics import Window
-from draw import Point, Line
+from draw import *
 
 def main():
     win = Window(800, 600)
 
-    point1 = Point(4, 8)
-    point2 = Point(300, 50)
-    point3 = Point(6, 11)
-    point4 = Point(200, 130)
+    #point1 = Point(4, 8)
+    #point2 = Point(300, 50)
+    #point3 = Point(6, 11)
+    #point4 = Point(200, 130)
 
-    line1 = Line(point1, point2)
-    line2 = Line(point3, point4)
+    #line1 = Line(point1, point2)
+    #line2 = Line(point3, point4)
 
-    win.draw_line(line1, "red")
-    win.draw_line(line2, "yellow")
+    #win.draw_line(line1, "red")
+    #win.draw_line(line2, "yellow")
+
+    cell1 = Cell(x1=5, y1=5, x2=20, y2=20, window=win)
+    cell2 = Cell(left_wall=False, x1=30, y1=5, x2=50, y2=20, window=win)
+    cell3 = Cell(top_wall=False, x1=60, y1=5, x2=80, y2=20, window=win)
+    cell4 = Cell(right_wall=False, x1=90, y1=5, x2=110, y2=20, window=win)
+    cell5 = Cell(bottom_wall=False, x1=120, y1=5, x2=140, y2=20, window=win)
+    cell6 = Cell(left_wall=False, bottom_wall=False, x1=150, y1=5, x2=180, y2=20, window=win)
+    cell7 = Cell(left_wall=False, top_wall=False, x1=190, y1=5, x2=210, y2=20, window=win)
+    cell8 = Cell(right_wall=False, top_wall=False, x1=220, y1=5, x2=240, y2=20, window=win)
+    cell9 = Cell(right_wall=False, bottom_wall=False, x1=250, y1=5, x2=270, y2=20, window=win)
+    cell10 = Cell(top_wall=False, bottom_wall=False, x1=280, y1=5, x2=300, y2=20, window=win)
+    cell11 = Cell(left_wall=False, right_wall=False, x1=310, y1=5, x2=330, y2=20, window=win)
+    cell12 = Cell(left_wall=False, right_wall=False, bottom_wall=False, x1=340, y1=5, x2=360, y2=20, window=win)
+    cell13 = Cell(right_wall=False, top_wall=False, bottom_wall=False, x1=370, y1=5, x2=390, y2=20, window=win)
+    cell14 = Cell(left_wall=False, right_wall=False, top_wall=False, x1=400, y1=5, x2=420, y2=20, window=win)
+    cell15 = Cell(left_wall=False, top_wall=False, bottom_wall=False, x1=430, y1=5, x2=450, y2=20, window=win)
+
+    cell1.draw()
+    cell2.draw()
+    cell3.draw()
+    cell4.draw()
+    cell5.draw()
+    cell6.draw()
+    cell7.draw()
+    cell8.draw()
+    cell9.draw()
+    cell10.draw()
+    cell11.draw()
+    cell12.draw()
+    cell13.draw()
+    cell14.draw()
+    cell15.draw()
 
     win.wait_for_close()
 
